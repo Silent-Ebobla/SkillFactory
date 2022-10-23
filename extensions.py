a7854d250af8524bcf6d714f8931e5326d673fb9
Silent-Ebobla <110719929+Silent-Ebobla@users.noreply.github.com>
2022-10-23 20:42:55 +0300

    Update extensions.py

--- extensions.py
+++ extensions.py
@@ -2,7 +2,7 @@ import json
 import requests
 from config import keys
 
-class ConvertedException(Exception):
+class APIException(Exception):
     pass
 
 class CryptoConverter:
@@ -10,22 +10,22 @@ class CryptoConverter:
     def get_price(quote: str, base: str, amount: str):
 
         if quote == base:
-            raise ConvertedException(f'Невозможно перевести одинаковые валюты {base}.')
+            raise APIException(f'Невозможно перевести одинаковые валюты {base}.')
 
         try:
             quote_ticker = keys[quote]
         except KeyError:
-            raise ConvertedException(f'Неудалось обработать валюту {quote}')
+            raise APIException(f'Неудалось обработать валюту {quote}')
 
         try:
             base_ticker = keys[base]
         except KeyError:
-            raise ConvertedException(f'Неудалось обработать валюту {base}')
+            raise APIException(f'Неудалось обработать валюту {base}')
 
         try:
             amount = float(amount)
         except ValueError:
-            raise ConvertedException(f'Не удалось обработать количество {amount}')
+            raise APIException(f'Не удалось обработать количество {amount}')
 
         r = requests.get(f'https://min-api.cryptocompare.com/data/price?fsym={quote_ticker}&tsyms={base_ticker}')
         total_base = json.loads(r.content)[keys[base]] * amount

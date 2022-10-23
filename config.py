538a7e6b3c73f0c06c5d6670bcc9fb8e7e6900d1
Silent-Ebobla <110719929+Silent-Ebobla@users.noreply.github.com>
2022-10-23 20:41:09 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,11 @@
+
+
+TOKEN = '5760088850:AAHwCXoMU4RIVRQNj-UBnOmpxVAxeeTW_Co'
+
+keys = {
+    'биткоин': 'BTC',
+    'эфириум': 'ETH',
+    'доллар': 'USD',
+    'рубль': 'RUB',
+    'йена': 'JPY',
+}
